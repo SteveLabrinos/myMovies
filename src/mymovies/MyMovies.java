60be3045573cfde78a76e42f71eb3fcbca162918
Steve <slabrinos9214@gmail.com>
2019-01-29 00:09:51 +0200

    Inital push to create the starting repo, by SteveLab 27-01-2019

---
+++ src/mymovies/MyMovies.java
@@ -0,0 +1,13 @@
+package mymovies;
+/**
+ *
+ * @author Labrinos
+ */
+import javax.swing.JOptionPane;
+public class MyMovies {
+
+    public static void main(String[] args) {
+        // first line of code to test work enviroment
+        JOptionPane.showMessageDialog(null, "Hello World, Steve");
+    } 
+}
