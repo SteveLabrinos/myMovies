748facaab1760318ea084613fe45abe8e3eb89be
SHADOW <SHADOW@192.168.1.20>
2019-01-29 19:05:38 +0200

    έχω αλλαξει το όνομα και το μύνημα

--- src/mymovies/MyMovies.java
+++ src/mymovies/MyMovies.java
@@ -12,7 +12,7 @@ public class MyMovies {
         final String INITMESSAGE="Hello Universe - ";
         
         //Steve's contribution to the file111
-        TestClass steve=new TestClass("Periklis");
+        TestClass steve=new TestClass("Peri");
         JOptionPane.showMessageDialog(null,INITMESSAGE.concat(steve.getName())); 
         //create your object then put your welcome message to the team here
     } 
