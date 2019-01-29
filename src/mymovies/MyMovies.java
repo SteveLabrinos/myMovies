2ce41675187c294e0284452947c8d50c9bef6edc
Steve <slabrinos9214@gmail.com>
2019-01-29 18:37:19 +0200

    added new Class TestClass. Changes made in Main

--- src/mymovies/MyMovies.java
+++ src/mymovies/MyMovies.java
@@ -11,7 +11,7 @@ public class MyMovies {
         //1rst change made
         final String INITMESSAGE="Hello World - ";
         
-        //Steve's contribution to the file
+        //Steve's contribution to the file111
         TestClass steve=new TestClass("Steve");
         JOptionPane.showMessageDialog(null,INITMESSAGE.concat(steve.getName())); 
         //create your object then put your welcome message to the team here
